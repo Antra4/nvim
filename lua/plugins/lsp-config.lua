02fa7e5c5ac24af53eff6b776e32a51cbec1c840
Anthony Flores <154497502+Antra4@users.noreply.github.com>
2025-10-27 18:18:44 -0700

    Added explicit hover keymap

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,6 +10,7 @@ return {
 		config = function()
 			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+			vim.keymap.set('n', 'K', vim.lsp.buf.definition, {})
 		end
 	},
 	{
