e863fa241d5a040b323dc1b3cce2a79da6336d52
Anthony Flores <anthonyflores@MacBookAir.attlocal.net>
2025-10-31 00:03:33 -0700

    Added vim-fugitive for git support and changed pane nav

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,12 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.mouse = 'a'
 
+-- Navigate Vim Panes Better
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
 -- Inline diagnostics
 -- vim.diagnostic.config({
 --   virtual_text = {
